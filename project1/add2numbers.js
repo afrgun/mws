3e83faacdbf0a04a2f4be90e151fdb653d28f939
afrgun <afrila.aries@gmail.com>
2018-09-29 15:29:01 +0700

    mws;

---
+++ project1/add2numbers.js
@@ -0,0 +1,16 @@
+function calc(){
+    let angka = document.querySelectorAll('input');
+    
+        let i1 = angka[0].value;
+        let i2 = angka[1].value;
+
+        angka[2].value = parseInt(i1) + parseInt(i2)
+}
+
+let tombol = document.querySelector('button');
+tombol.addEventListener('click', calc);
+
+function hasil(){
+    console.log("hasil")
+    return calc()
+}
